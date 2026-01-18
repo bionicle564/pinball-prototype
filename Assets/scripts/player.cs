563c10b337de73204339ffd2ad8247e6ed4efae8
bionicle564 <57322673+bionicle564@users.noreply.github.com>
2026-01-17 23:24:19 -0500

    basic demo level (all but key)

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -43,6 +43,7 @@ public class player : MonoBehaviour
 
         }
 
+        //launches right
         if (left.WasPressedThisFrame())
         {
             rb.linearVelocity = new Vector3(0, 0, 0);
@@ -51,6 +52,7 @@ public class player : MonoBehaviour
 
         }
 
+        //launches left
         if (right.WasPressedThisFrame())
         {
             rb.linearVelocity = new Vector3(0, 0, 0);
@@ -58,8 +60,9 @@ public class player : MonoBehaviour
             rb.AddForce(force * 10 * rb.mass * speed , ForceMode.Impulse);
         }
 
+        //update the camera
         cam.transform.position = this.transform.position + (camOffset * cameraOffsetScale);
-        cam.transform.LookAt(this.transform);
+        cam.transform.LookAt(this.transform, up);
 
         // make the ball fall
         rb.AddForce(-up * rb.mass * 40);
@@ -67,8 +70,8 @@ public class player : MonoBehaviour
         //push the ball against the table
         rb.AddForce(-grav * rb.mass * 20);
 
+        //an attempt to give the player a threshold of minimum speed
         Vector3 totalForce = rb.linearVelocity;
-
         if (totalForce.magnitude < .1f)
         {
             rb.linearVelocity = new Vector3(0, 0, 0);
@@ -77,27 +80,25 @@ public class player : MonoBehaviour
 
     }
 
-    public void ChangeGrav(Vector3 ballUp, Vector3 camUp)
+    //changes the player's reletive gravety, and changes the camera to match
+    public void ChangeGrav(Vector3 ballUp, Vector3 camUp, bool changeCam = true)
     {
+        rb.linearVelocity = new Vector3(0, 0, 0);
         //find the fixed angle for camera
         float camAngle = Vector3.Angle(new Vector3(0,1,0), cameraOffsetPosition);
 
         up = ballUp;
         grav = camUp;
 
-        Vector3 right = Vector3.Cross(grav, up);
+        if (changeCam)
+        {
+            Vector3 right = Vector3.Cross(grav, up);
 
+            // get new up length of offset
+            Vector3 tmp = Vector3.Normalize(grav) * cameraOffsetPosition.magnitude;
 
-        // get new up length of offset
-        Vector3 tmp = Vector3.Normalize(grav) * cameraOffsetPosition.magnitude;
-
-        //rotate it fixed degrees
-        camOffset = Quaternion.AngleAxis(camAngle, Vector3.Cross(up, grav)) * tmp;
-
-        //this doesn't work for some reason
-        //cam.transform.LookAt(this.transform.position);
-        
-        //Quaternion.AngleAxis(camAngle, Vector3.Cross(up, grav));
-
+            //rotate it fixed degrees
+            camOffset = Quaternion.AngleAxis(camAngle, Vector3.Cross(up, grav)) * tmp;
+        }
     }
 }
